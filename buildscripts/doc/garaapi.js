98452f0a5e60bd917d2a79e2eeceabce3a4264cf
gossi <mail@gossimaniac.net>
2007-12-09 20:45:49 +0000

    Deleting old resources

--- buildscripts/doc/garaapi.js
+++
@@ -1,6 +0,0 @@
-function selectNamespace (namespace) {
-	if (parent.namespace && parent.main) {
-		parent.namespace.location.href = namespace + "._nav.html";
-		parent.main.location.href = namespace + "._namespace.html";
-	}
-}
